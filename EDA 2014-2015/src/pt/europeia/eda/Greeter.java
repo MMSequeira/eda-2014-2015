5551839209b2afda485d53257238231f583ab70b
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2015-02-26 12:32:27 +0000

    New Greeter class.

---
+++ EDA 2014-2015/src/pt/europeia/eda/Greeter.java
@@ -0,0 +1,9 @@
+package pt.europeia.eda;
+
+import static java.lang.System.out;
+
+public class Greeter {
+    public static void main(String[] arguments) {
+        out.println("Hello world!");
+    }
+}
