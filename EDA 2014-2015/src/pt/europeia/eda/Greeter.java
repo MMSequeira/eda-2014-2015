b8f9a2142267763a93b814e17316bffe9a5981c5
Manuel Menezes de Sequeira <MMSequeira@gmail.com>
2015-02-26 12:34:14 +0000

    Improved implementation.

--- EDA 2014-2015/src/pt/europeia/eda/Greeter.java
+++ EDA 2014-2015/src/pt/europeia/eda/Greeter.java
@@ -4,6 +4,6 @@ import static java.lang.System.out;
 
 public class Greeter {
     public static void main(String[] arguments) {
-        out.println("Hello world!");
+        out.println("Hello Git world!");
     }
 }
