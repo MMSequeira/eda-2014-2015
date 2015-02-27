3928085eeb94124561eddf2866ae476a509681e2
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-02-27 17:17:36 +0000

    Add blank line at the start and end of the classes.

--- EDA 2014-2015/src/pt/europeia/eda/java_slides/Greeter.java
+++ EDA 2014-2015/src/pt/europeia/eda/java_slides/Greeter.java
@@ -3,7 +3,9 @@ package pt.europeia.eda.java_slides;
 import static java.lang.System.out;
 
 public class Greeter {
+
     public static void main(final String[] arguments) {
         out.println("Hello GitHub world!");
     }
+
 }
