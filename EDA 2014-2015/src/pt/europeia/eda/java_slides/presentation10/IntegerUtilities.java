1289199052ddd4e405ad173b48c3500e64a44cef
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-03-01 18:09:02 +0000

    Add package and example classes corresponding to presentation 10.

---
+++ EDA 2014-2015/src/pt/europeia/eda/java_slides/presentation10/IntegerUtilities.java
@@ -0,0 +1,55 @@
+package pt.europeia.eda.java_slides.presentation10;
+
+public class IntegerUtilities {
+
+    private IntegerUtilities() {
+        throw new RuntimeException("Attempt to instantiate package-class");
+    }
+    
+    /**
+     * Returns the greatest common divisor (GCD) of two integer numbers.
+     * 
+     * @param m
+     *            first of the two integer numbers whose GCD will be returned.
+     * @param n
+     *            second of the two integer numbers whose GCD will be returned.
+     * @return the greatest common divisor of m and n.
+     * @pre m ≠ 0 ∨ n ≠ 0
+     * @post gcd = gcd(m, n)
+     */
+    public static int gcd(int m, int n) {
+        if (m == 0 && n == 0)
+            throw new IllegalArgumentException("Illegal call with "
+                    + "m = 0 and n = 0.");
+
+        m = Math.abs(m);
+        n = Math.abs(n);
+
+        while (m != 0) {
+            int temporary = m;
+            m = n % m;
+            n = temporary;
+        }
+
+        return n;
+    }
+//  public static int gcd(int m, int n) {
+//  if (m == 0 && n == 0)
+//      throw new IllegalArgumentException("Illegal call with "
+//              + "m = 0 and n = 0.");
+//
+//  m = Math.abs(m);
+//  n = Math.abs(n);
+//
+//  if (m == 0 || n == 0)
+//      return Math.max(m, n);
+//
+//  int possibleGcd = Math.min(m, n);
+//
+//  while (m % possibleGcd != 0 || n % possibleGcd != 0)
+//      possibleGcd--;
+//
+//  return possibleGcd;
+//}
+
+}
---
+++ EDA 2014-2015/src/pt/europeia/eda/java_slides/presentation10/IntegerUtilitiesTester.java
@@ -0,0 +1,17 @@
+package pt.europeia.eda.java_slides.presentation10;
+
+import static java.lang.System.out;
+import static pt.europeia.eda.java_slides.presentation10.IntegerUtilities.gcd;
+
+public class IntegerUtilitiesTester {
+
+    public static void main(final String[] arguments) {
+        out.println("gcd(20, 25) is " + gcd(20, 25));
+        out.println("gcd(-20, 25) is " + gcd(-20, 25));
+        out.println("gcd(20, -25) is " + gcd(20, -25));
+        out.println("gcd(-20, -25) is " + gcd(-20, -25));
+        out.println("gcd(0, 25) is " + gcd(0, 25));
+        out.println("gcd(20, 0) is " + gcd(20, 0));
+    }
+
+}
