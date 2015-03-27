f85a45308c823b67148cc21c563ae03f477ab6e0
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-03-27 19:29:58 +0000

    Add GPL comment.

--- EDA 2014-2015/src/pt/europeia/eda/session09/AutoboxingTester.java
+++ EDA 2014-2015/src/pt/europeia/eda/session09/AutoboxingTester.java
@@ -18,5 +18,32 @@ public class AutoboxingTester {
         compare(43, 43);
         compare(142, 142);
     }
-    
+
 }
+
+/*******************************************************************************
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ *
+ * This file is a derivative work of the code which accompanies the textbook
+ *
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ *
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ ******************************************************************************/
