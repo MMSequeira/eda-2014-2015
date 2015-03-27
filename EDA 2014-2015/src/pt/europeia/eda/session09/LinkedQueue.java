3d9c0e2646f2b5341028eb28f49f655b4be68d03
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-03-27 17:39:06 +0000

    Add code for session 9.

---
+++ EDA 2014-2015/src/pt/europeia/eda/session09/LinkedQueue.java
@@ -0,0 +1,123 @@
+package pt.europeia.eda.session09;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static pt.europeia.eda.Tools.in;
+import static pt.europeia.eda.Tools.out;
+
+public class LinkedQueue<Item> implements Iterable<Item> {
+
+    private Node first, last;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void enqueue(final Item item) {
+        final Node oldLast = last;
+
+        last = new Node();
+
+        last.item = item;
+        last.next = null;
+
+        if (isEmpty())
+            first = last;
+        else
+            oldLast.next = last;
+    }
+
+    public Item dequeue() {
+        if (isEmpty())
+            throw new NoSuchElementException("Queue underflow.");
+
+        final Item item = first.item;
+
+        first = first.next;
+
+        if (isEmpty())
+            last = null;
+
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException("No more items in iteration.");
+
+            final Item item = current.item;
+
+            current = current.next;
+
+            return item;
+        }
+
+    }
+
+    public static void main(final String[] arguments) {
+        final LinkedQueue<String> words = new LinkedQueue<String>();
+
+        while (in.hasNext()) {
+            final String word = in.next();
+            if (word.equals("-"))
+                out.print(words.dequeue() + " ");
+            else
+                words.enqueue(word);
+        }
+        out.println();
+
+        out.println("Remaining words:");
+        for (String word : words)
+            out.println(word);
+    }
+
+}
+
+/*************************************************************************
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ *
+ * This file is a derivative work of the code which accompanies the textbook
+ *
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ *
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ *************************************************************************/
---
+++ EDA 2014-2015/src/pt/europeia/eda/session09/ResizingArrayStack.java
@@ -0,0 +1,124 @@
+package pt.europeia.eda.session09;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static pt.europeia.eda.Tools.in;
+import static pt.europeia.eda.Tools.out;
+
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+    private Item[] items;
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
+    public ResizingArrayStack() {
+        items = (Item[]) new Object[1];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(final Item item) {
+        if (size == items.length)
+            resizeTo(2 * items.length);
+        items[size] = item;
+        size++;
+    }
+
+    public Item pop() {
+        if (isEmpty())
+            throw new NoSuchElementException("Stack underflow.");
+
+        size--;
+
+        final Item item = items[size];
+        items[size] = null;
+
+        if (size > 0 && size == items.length / 4)
+            resizeTo(items.length / 2);
+
+        return item;
+    }
+
+    private void resizeTo(final int newCapacity) {
+        @SuppressWarnings("unchecked")
+        final Item[] copyOfItems = (Item[]) new Object[newCapacity];
+
+        for (int i = 0; i != size; i++)
+            copyOfItems[i] = items[i];
+
+        items = copyOfItems;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+
+        private int current = size;
+
+        public boolean hasNext() {
+            return current > 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext())
+                throw new NoSuchElementException("No more items in iteration.");
+
+            --current;
+
+            return items[current];
+        }
+
+    }
+
+    public static void main(String[] arguments) {
+        final ResizingArrayStack<String> words = new ResizingArrayStack<String>();
+
+        while (in.hasNext()) {
+            final String word = in.next();
+            if (word.equals("-"))
+                out.print(words.pop() + " ");
+            else
+                words.push(word);
+        }
+        out.println();
+
+        out.println("Remaining words:");
+        for (String word : words)
+            out.println(word);
+    }
+}
+
+/*************************************************************************
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ *
+ * This file is a derivative work of the code which accompanies the textbook
+ *
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ *
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ *************************************************************************/
