aae7dded73b2e21a13c7aad05f46373280db5187
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-03-27 17:39:22 +0000

    Add code for session 10.

---
+++ EDA 2014-2015/src/pt/europeia/eda/session10/MM1QueueSimulation.java
@@ -0,0 +1,60 @@
+package pt.europeia.eda.session10;
+
+import pt.europeia.eda.Histogram;
+import pt.europeia.eda.session09.LinkedQueue;
+import edu.princeton.cs.introcs.StdRandom;
+
+public class MM1QueueSimulation {
+
+    public static void main(final String[] arguments) {
+        final double lambda = Double.parseDouble(arguments[0]); // arrival rate
+        final double mu = Double.parseDouble(arguments[1]); // service rate
+        double nextArrival = StdRandom.exp(lambda);
+        double nextService = nextArrival + StdRandom.exp(mu);
+
+        final LinkedQueue<Double> queue = new LinkedQueue<Double>();
+        final Histogram hist = new Histogram("M/M/1 Queue", 61);
+
+        while (true) {
+            while (nextArrival < nextService) {
+                queue.enqueue(nextArrival);
+                nextArrival += StdRandom.exp(lambda);
+            }
+
+            final double arrival = queue.dequeue();
+            final double wait = nextService - arrival;
+            hist.addDataPoint(Math.min(60, (int) (Math.round(wait))));
+            if (queue.isEmpty())
+                nextService = nextArrival + StdRandom.exp(mu);
+            else
+                nextService = nextService + StdRandom.exp(mu);
+        }
+    }
+}
+
+/*************************************************************************
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ *
+ * This file is a derivative work of the code which accompanies the textbook
+ *
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ *
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ *
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ *************************************************************************/
