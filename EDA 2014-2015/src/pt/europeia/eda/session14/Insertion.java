a0a7b168eaf38855c9153ccd615f0b15ff4f7708
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-04-16 11:26:31 +0100

    Add GPL comment. Change style checker option for method type parameters.

--- EDA 2014-2015/src/pt/europeia/eda/session14/Insertion.java
+++ EDA 2014-2015/src/pt/europeia/eda/session14/Insertion.java
@@ -8,7 +8,7 @@ public class Insertion {
             for (int i = numberOfSortedItems; i != 0
                     && isLess(values[i], values[i - 1]); i--)
                 swap(values, i - 1, i);
-        
+
         assert isIncreasing(values) : "Array should be increasing after sorting.";
     }
 
@@ -18,10 +18,11 @@ public class Insertion {
     }
 
     private static <Item extends Comparable<? super Item>> void swap(
-            final Item[] values, final int i, final int j) {
-        final Item temporary = values[i];
-        values[i] = values[j];
-        values[j] = temporary;
+            final Item[] values, final int firstPosition,
+            final int secondPosition) {
+        final Item temporary = values[firstPosition];
+        values[firstPosition] = values[secondPosition];
+        values[secondPosition] = temporary;
     }
 
     private static <Item extends Comparable<? super Item>> boolean isIncreasing(
@@ -33,3 +34,30 @@ public class Insertion {
     }
 
 }
+
+/*
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ * 
+ * This file is a derivative work of the code which accompanies the textbook
+ * 
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ * 
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ */
--- EDA 2014-2015/src/pt/europeia/eda/session14/Selection.java
+++ EDA 2014-2015/src/pt/europeia/eda/session14/Selection.java
@@ -6,14 +6,14 @@ public class Selection {
             final Item[] values) {
         for (int numberOfSortedItems = 0; numberOfSortedItems < values.length - 1; numberOfSortedItems++) {
             int indexOfMinimum = numberOfSortedItems;
-            
+
             for (int i = indexOfMinimum + 1; i != values.length; i++)
                 if (isLess(values[i], values[indexOfMinimum]))
                     indexOfMinimum = i;
-            
+
             swap(values, numberOfSortedItems, indexOfMinimum);
         }
-        
+
         assert isIncreasing(values) : "Array should be increasing after sorting.";
     }
 
@@ -23,10 +23,11 @@ public class Selection {
     }
 
     private static <Item extends Comparable<? super Item>> void swap(
-            final Item[] values, final int i, final int j) {
-        final Item temporary = values[i];
-        values[i] = values[j];
-        values[j] = temporary;
+            final Item[] values, final int firstPosition,
+            final int secondPosition) {
+        final Item temporary = values[firstPosition];
+        values[firstPosition] = values[secondPosition];
+        values[secondPosition] = temporary;
     }
 
     private static <Item extends Comparable<? super Item>> boolean isIncreasing(
@@ -37,3 +38,30 @@ public class Selection {
         return true;
     }
 }
+
+/*
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ * 
+ * This file is a derivative work of the code which accompanies the textbook
+ * 
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ * 
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ */
