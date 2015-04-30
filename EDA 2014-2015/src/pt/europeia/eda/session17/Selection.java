9c6b04010d462c2ac8a634db78bdb4a56ad0aa5c
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-04-30 10:54:59 +0100

    Add new Selection class with both natural order and comparator based sorting methods.

---
+++ EDA 2014-2015/src/pt/europeia/eda/session17/Selection.java
@@ -0,0 +1,101 @@
+package pt.europeia.eda.session17;
+
+import java.util.Comparator;
+
+public final class Selection {
+
+    private Selection() {
+        throw new RuntimeException("Attempt to instantiate package-class");
+    }
+
+    public static <Item extends Comparable<? super Item>> void sort(
+            final Item[] values) {
+        for (int numberOfSortedItems = 0; numberOfSortedItems < values.length - 1; numberOfSortedItems++) {
+            int indexOfMinimum = numberOfSortedItems;
+
+            for (int i = indexOfMinimum + 1; i != values.length; i++)
+                if (isLess(values[i], values[indexOfMinimum]))
+                    indexOfMinimum = i;
+
+            swap(values, numberOfSortedItems, indexOfMinimum);
+        }
+
+        assert isIncreasing(values) : "Array should be increasing after sorting.";
+    }
+
+    public static <Item> void sort(final Item[] values,
+            final Comparator<? super Item> comparator) {
+        for (int numberOfSortedItems = 0; numberOfSortedItems < values.length - 1; numberOfSortedItems++) {
+            int indexOfMinimum = numberOfSortedItems;
+
+            for (int i = indexOfMinimum + 1; i != values.length; i++)
+                if (isLess(values[i], values[indexOfMinimum], comparator))
+                    indexOfMinimum = i;
+
+            swap(values, numberOfSortedItems, indexOfMinimum);
+        }
+
+        assert isIncreasing(values, comparator) : "Array should be increasing after sorting.";
+    }
+
+    private static <Value extends Comparable<? super Value>> boolean isLess(
+            final Value first, final Value second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private static <Value> boolean isLess(final Value first,
+            final Value second, final Comparator<? super Value> comparator) {
+        return comparator.compare(first, second) < 0;
+    }
+
+    private static void swap(final Object[] values, final int firstPosition,
+            final int secondPosition) {
+        final Object temporary = values[firstPosition];
+        values[firstPosition] = values[secondPosition];
+        values[secondPosition] = temporary;
+    }
+
+    private static <Item extends Comparable<? super Item>> boolean isIncreasing(
+            final Item[] values) {
+        for (int i = 1; i < values.length; i++)
+            if (isLess(values[i], values[i - 1]))
+                return false;
+        return true;
+    }
+
+    private static <Item> boolean isIncreasing(final Item[] values,
+            final Comparator<? super Item> comparator) {
+        for (int i = 1; i < values.length; i++)
+            if (isLess(values[i], values[i - 1], comparator))
+                return false;
+        return true;
+    }
+
+}
+
+/*
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ * 
+ * This file is a derivative work of the code which accompanies the textbook
+ * 
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ * 
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ */
