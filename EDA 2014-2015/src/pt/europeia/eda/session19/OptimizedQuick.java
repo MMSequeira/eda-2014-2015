ee131f6b6654d1bdf9aefcf0765413ff2690043d
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2015-05-05 14:06:33 +0100

    Add OptimizedQuick to session 19.

---
+++ EDA 2014-2015/src/pt/europeia/eda/session19/OptimizedQuick.java
@@ -0,0 +1,191 @@
+package pt.europeia.eda.session19;
+
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdRandom;
+
+public final class OptimizedQuick {
+
+    private static final int cutoff = 7;
+
+    private OptimizedQuick() {
+        throw new RuntimeException("Attempt to instantiate package-class");
+    }
+
+    public static <Item extends Comparable<? super Item>> void sort(
+            final Item[] values) {
+        StdRandom.shuffle(values);
+
+        sort(values, 0, values.length - 1);
+
+        assert isIncreasing(values) : "Array should be increasing after sorting.";
+    }
+
+    private static <Item extends Comparable<? super Item>> void sort(
+            final Item[] values, final int first, final int last) {
+        if (last - first < cutoff) {
+            insertionSort(values, first, last);
+
+            return;
+        }
+
+        final int middle = first + (last - first) / 2;
+        final int positionOfMedian = positionOfMedianOf3(values, first, middle,
+                last);
+        swap(values, first, positionOfMedian);
+
+        final int j = partition(values, first, last);
+        sort(values, first, j - 1);
+        sort(values, j + 1, last);
+
+        assert isIncreasing(values, first, last) : "Array segment should be increasing after sorting.";
+    }
+
+    private static <Item extends Comparable<? super Item>> int partition(
+            final Item[] values, final int first, final int last) {
+        int i = first;
+        int j = last + 1;
+
+        final Item pivot = values[first];
+
+        while (true) {
+
+            do
+                ++i;
+            while (isLess(values[i], pivot) && i != last);
+
+            do
+                --j;
+            while (isLess(pivot, values[j]) && j != first);
+
+            if (i >= j)
+                break;
+
+            swap(values, i, j);
+        }
+
+        swap(values, first, j);
+
+        return j;
+    }
+
+    public static <Item extends Comparable<? super Item>> Item select(
+            final Item[] values, final int k) {
+        if (k < 0 || values.length <= k)
+            throw new IndexOutOfBoundsException(
+                    "Selected element out of bounds");
+
+        StdRandom.shuffle(values);
+
+        int first = 0;
+        int last = values.length - 1;
+
+        while (last > first) {
+            final int j = partition(values, first, last);
+            if (j > k)
+                last = j - 1;
+            else if (j < k)
+                first = j + 1;
+            else
+                return values[j];
+        }
+
+        return values[first];
+    }
+
+    private static <Item extends Comparable<? super Item>> void insertionSort(
+            final Item[] values, final int first, final int last) {
+        for (int i = first + 1; i <= last; i++)
+            for (int j = i; j > first && isLess(values[j], values[j - 1]); j--)
+                swap(values, j, j - 1);
+    }
+
+    private static <Value extends Comparable<? super Value>> boolean isLess(
+            final Value first, final Value second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private static void swap(final Object[] values, final int i, final int j) {
+        final Object temporary = values[i];
+        values[i] = values[j];
+        values[j] = temporary;
+    }
+
+    private static <Item extends Comparable<? super Item>> int positionOfMedianOf3(
+            final Item[] values, final int i, final int j, final int k) {
+        if (isLess(values[i], values[j]))
+            if (isLess(values[j], values[k]))
+                return j;
+            else if (isLess(values[i], values[k]))
+                return k;
+            else
+                return i;
+        else if (isLess(values[k], values[j]))
+            return j;
+        else if (isLess(values[k], values[i]))
+            return k;
+        else
+            return i;
+    }
+
+    private static <Item extends Comparable<? super Item>> boolean isIncreasing(
+            final Item[] values) {
+        return isIncreasing(values, 0, values.length - 1);
+    }
+
+    private static <Item extends Comparable<? super Item>> boolean isIncreasing(
+            final Item[] a, final int first, final int last) {
+        for (int i = first + 1; i <= last; i++)
+            if (isLess(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    private static void show(final Object[] values) {
+        for (Object value : values)
+            StdOut.println(value);
+    }
+
+    public static void main(final String[] arguments) {
+        final In in = new In(arguments[0]);
+        final String[] words = in.readAllStrings();
+        OptimizedQuick.sort(words);
+        show(words);
+
+        StdRandom.shuffle(words);
+
+        StdOut.println();
+        for (int i = 0; i != words.length; i++) {
+            final String ith = OptimizedQuick.select(words, i);
+            StdOut.println(ith);
+        }
+    }
+
+}
+
+/*
+ * Copyright 2015, Robert Sedgewick and Kevin Wayne.
+ * 
+ * Copyright 2015, Manuel Menezes de Sequeira.
+ * 
+ * This file is a derivative work of the code which accompanies the textbook
+ * 
+ * Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne, Addison-Wesley
+ * Professional, 2011, ISBN 0-321-57351-X. http://algs4.cs.princeton.edu
+ * 
+ * This code is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this code. If not, see http://www.gnu.org/licenses.
+ * 
+ * Any errors found in this code should be assumed to be the responsibility of
+ * the author of the modifications to the original code (viz. Manuel Menezes de
+ * Sequeira).
+ */
